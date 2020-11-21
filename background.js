a6c0c3af854930bae5b159612313bdfb54cb098d
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2020-11-21 19:20:48 +0900

    preferヘッダーを上書きする機能を追加

--- background.js
+++ background.js
@@ -1,10 +1,37 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
-        })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "localhost" },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
+  chrome.webRequest.onBeforeSendHeaders.addListener(
+    async function (details) {
+      let found = false;
+      for (let i = 0; i < details.requestHeaders.length; ++i) {
+        if (details.requestHeaders[i].name.toLowerCase() === "prefer") {
+          details.requestHeaders[i].value = "testValue";
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        details.requestHeaders.push({
+          name: "Prefer",
+          value: "testValue",
+        });
+      }
+      return {
+        requestHeaders: details.requestHeaders,
+      };
+    },
+    { urls: ["<all_urls>"] },
+    ["blocking", "requestHeaders"]
+  );
 });
